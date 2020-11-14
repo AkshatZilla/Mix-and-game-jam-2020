b8c3fe5cdc23fa41eaf542896b8a2cd98f319c32
Akshat Nedungottil <akshatnedungottil@gmail.com>
2020-11-14 23:41:30 +0530

    Changes made

---
+++ Assets/BulletMovement.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletMovement : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    [SerializeField] private float bulletSpeed = 5f;
+    public GameObject bulletPrefab;
+    
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+    
+    void FixedUpdate()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            Shoot();
+        }
+    }
+    
+    void Shoot()
+    {
+        rb.AddForce(transform.forward * bulletSpeed);
+    }
+}
