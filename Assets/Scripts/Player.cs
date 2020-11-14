1edbb29113a19461c80e992bee3bea5bf1416cba
Akshat Nedungottil <akshatnedungottil@gmail.com>
2020-11-14 15:48:11 +0530

    CHANGES!!!

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,15 +4,13 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
+    public GameObject bulletPrefab;
+    
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Instantiate(bulletPrefab, transform.position, transform.rotation);
+        }
     }
 }
---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    public Transform[] spawnPoints;
+    public GameObject slimePrefab;
+    public float timeBetweenWaves = 1f;
+    float timeToSpawn = 2f;
+
+    void Update()
+    {
+        if (Time.time >= timeToSpawn)
+        {
+            SpawnSlimes();
+            timeToSpawn = Time.time + timeBetweenWaves;
+        }
+    }
+
+    void SpawnSlimes()
+    {
+        int randomIndex = Random.Range(0, spawnPoints.Length);
+
+        for (int i = 0; i < spawnPoints.Length; i++)
+        {
+            if (randomIndex != i)
+            {
+                Instantiate(slimePrefab, spawnPoints[i].position, Quaternion.identity);
+            }
+        }
+    }
+}
